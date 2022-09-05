a83cc614b24b767dbc050d30059d729d5cd47835
Kevin Kinner <kevin-kinner@web.de>
2022-09-05 10:33:56 +0200

    Table Creation

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let div = document.createElement("div");
-div.textContent = "this is just a test";
+
+function creator(){
+    let table = document.createElement("table");
+    document.getElementById("mazewrapper").appendChild(table);
+    
+    //create Table
+    while (rowIndex<mazeHeight) {
+        let tr = document.createElement("tr");
+        tr.id = "row " + rowIndex;
+        table.appendChild(tr);
+        
+        while (columnIndex<mazeWidth) {
+            let cell = document.createElement("td");
+            cell.textContent = "test";
+            document.getElementById("row " + rowIndex).appendChild(cell)
+            columnIndex++;
+        }
+        columnIndex = 0;
+        rowIndex++;
+    }
+}
 
 
-document.getElementById("mazewrapper").appendChild(div);
+let rowIndex = 0;
+let columnIndex = 0;
+let mazeWidth = 10;
+let mazeHeight = 10;
+document.getElementById("startbutton").addEventListener("click", creator);
